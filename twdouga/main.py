15ab3b9c502702ed0b4819acacc64e3b8ec80d6b
tamanobi <tamanobi@gmail.com>
2021-09-24 04:23:40 +0900

    feat: cors

--- twdouga/main.py
+++ twdouga/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 import requests
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Twitter:
     _API_BASE = 'https://api.twitter.com/1.1/'
