192715c8e3565ec9e85ac1cf0df91ad64f245bef
tamanobi <tamanobi@gmail.com>
2021-09-24 14:17:38 +0900

    feat: error handling

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -20,25 +20,29 @@
         <a-breadcrumb-item>App</a-breadcrumb-item> -->
       </a-breadcrumb>
       <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
-        <a-input v-model="inputUrl" placeholder="twitter の url 例: https://twitter.com/hybridcIena/status/1441113107598770178" :disabled="loading" size="large">
-          <a-icon slot="prefix" type="twitter" />
-          <a-tooltip slot="suffix" title="Extra information">
-            <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
-          </a-tooltip>
-        </a-input>
-        <br />
-        <br />
-        <a-button type="primary" size="large" :loading="loading" @click="getVideo">動画のURLを手に入れる</a-button>
-        <br />
-        <br />
-        <div v-if="videoUrl">
-          <video controls style="max-width: 100%;">
-            <source :src="videoUrl" type="video/mp4"/>
-          </video>
-          <p>
-            <a :href="videoUrl">{{ videoUrl }}</a>
-          </p>
-        </div>
+        <a-row type="flex" justify="center" align="top">
+          <a-col :xs="24" :sm="24" :md="24" :lg="12">
+            <a-input v-model="inputUrl" allow-clear placeholder="twitter の url 例: https://twitter.com/hybridcIena/status/1441113107598770178" :disabled="loading" size="large">
+              <a-icon slot="prefix" type="twitter" />
+            </a-input>
+            <br />
+            <br />
+            <a-button type="primary" size="large" :loading="loading" @click="getVideo">動画のURLを手に入れる</a-button>
+            <br />
+            <br />
+            <a-spin :spinning="loading">
+              <div class="spin-content">
+                <div v-if="videoUrl">
+                  <video controls style="max-width: 100%;">
+                    <source :src="videoUrl" type="video/mp4"/>
+                  </video>
+                  <p><a :href="videoUrl">{{ videoUrl }}</a></p>
+                </div>
+                <div v-else>ここに動画が表示されます</div>
+              </div>
+            </a-spin>
+          </a-col>
+        </a-row>
       </div>
     </a-layout-content>
     <a-layout-footer style="text-align: center">
@@ -58,10 +62,18 @@ export default {
   },
   methods: {
     async getVideo() {
+      if (this.inputUrl === '' || !this.inputUrl.startsWith("https://") || !this.inputUrl.includes("twitter.com/") || !this.inputUrl.includes("status/")) {
+        this.$message.error('Twitter の URL を入力してください');
+        return;
+      }
       this.loading = true;
       const endpoint = process.env.ENDPOINT;
       const url = `${endpoint}?url=${encodeURIComponent(this.inputUrl)}`;
 			const response = await this.$axios.$get(url);
+      if (!response) {
+        this.$message.error('予期しないエラーが発生しました。しばらく経ってからお試しください');
+        return;
+      }
       this.loading = false;
       this.videoUrl = response.url;
       this.inputUrl = "";
@@ -78,5 +90,10 @@ export default {
   float: left;
   vertical-align: top;
   color: white;
+}
+.spin-content {
+  border: 1px solid #91d5ff;
+  background-color: #e6f7ff;
+  padding: 30px;
 }
 </style>
