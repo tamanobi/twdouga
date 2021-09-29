53b125422ac6e9b0e32b8707a46685609cf0a49c
tamanobi <tamanobi@gmail.com>
2021-09-30 04:58:26 +0900

    fix: skip when response is None

--- twdouga/batch/migrate_thumbnail.py
+++ twdouga/batch/migrate_thumbnail.py
@@ -7,6 +7,8 @@ session = sessionmaker(bind=engine)()
 # requests = session.query(Request).filter(and_(Request.thumbnail_url == "", Request.response is None))
 requests = session.query(Request).filter(or_(Request.thumbnail_url == None, Request.thumbnail_url == "")).all()
 for r in requests:
+    if r.response is None:
+        continue
     media = r.response.get("extended_entities", {}).get("media", [{}])[0]
     thumbnail_url = media.get("media_url_https", "")
     r.thumbnail_url = thumbnail_url
