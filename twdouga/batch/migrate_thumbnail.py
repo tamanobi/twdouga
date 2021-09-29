b4a5e175d89f4064e170e14d14286b986795da84
tamanobi <tamanobi@gmail.com>
2021-09-30 04:53:57 +0900

    feat: fill blank thumbnail url

---
+++ twdouga/batch/migrate_thumbnail.py
@@ -0,0 +1,13 @@
+from twdouga.db import engine
+from twdouga.models import Request
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import desc, and_, or_
+
+session = sessionmaker(bind=engine)()
+# requests = session.query(Request).filter(and_(Request.thumbnail_url == "", Request.response is None))
+requests = session.query(Request).filter(or_(Request.thumbnail_url == None, Request.thumbnail_url == "")).all()
+for r in requests:
+    media = r.response.get("extended_entities", {}).get("media", [{}])[0]
+    thumbnail_url = media.get("media_url_https", "")
+    r.thumbnail_url = thumbnail_url
+session.commit()
