8985683e4535c93cc904eb3442011e2bf49414b1
tamanobi <tamanobi@gmail.com>
2021-10-20 09:21:37 +0900

    refactor: typing

--- batch/upload_to_wasabi.py
+++ batch/upload_to_wasabi.py
@@ -4,6 +4,22 @@ import requests
 import hashlib
 import os
 import io
+from abc import ABC, abstractmethod
+
+
+class Uploader(ABC):
+    @abstractmethod
+    def upload(self, body: io.BytesIO, name: str):
+        pass
+
+class S3Uploader(Uploader):
+    def __init__(self, s3_client) -> None:
+        self.client = s3_client
+
+    def upload(self, body: io.BytesIO, name: str):
+        path = name[0:2] + "/" + name  # 分散のために名前の先頭二文字をフォルダとする
+        self.client.upload_fileobj(body, path)
+
 
 def get_bucket():
     s3 = boto3.resource('s3',
@@ -20,24 +36,24 @@ def get_json(offset: int):
     res = requests.get(API.format(offset))
     return res.json()
 
-class MyVideo:
+class VideoInfoExtractor:
     def __init__(self, obj) -> None:
         self.obj = obj
         self._binary = None
     
     @property
-    def url(self):
+    def url(self) -> str:
         return self.obj["video_url"]
     
     @property
-    def binary(self):
+    def binary(self) -> bytes:
         if self._binary is None:
             res = requests.get(self.url)
             self._binary = res.content
         return self._binary
 
     @property
-    def filename(self):
+    def filename(self) -> str:
         from pathlib import Path
 
         path = Path(self.url)
@@ -46,13 +62,12 @@ class MyVideo:
             suffix = suffix[0:suffix.find("?")]
         
         stem = hashlib.sha256(self.binary).hexdigest()
-        return stem[0:2] + "/" + stem + suffix
+        return stem + suffix
 
-bucket = get_bucket()
+uploader = S3Uploader(get_bucket())
 
-videos = [MyVideo(x) for x in get_json(0)]
-for offset in range(0, 100, 10):
+for offset in range(0, 10, 10):
     for row in get_json(offset):
-        vid = MyVideo(row)
+        vid = VideoInfoExtractor(row)
         with io.BytesIO(vid.binary) as f:
-            bucket.upload_fileobj(f, vid.filename)
+            uploader.upload(f, vid.filename)
